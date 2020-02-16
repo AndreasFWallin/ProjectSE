14cab123acc996db5e1e40ae566c80a27a180672
jadali17 <32588749+jadali17@users.noreply.github.com>
2020-02-16 18:08:56 +0100

    Update test_tournament.py

--- tests/test_tournament.py
+++ tests/test_tournament.py
@@ -1,4 +1,6 @@
 from projectse.tournament import Tournament
+from projectse.round import Round, Match
+from projectse.player import Player  
 import unittest
 
 
@@ -6,6 +8,15 @@ class TournamentTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
+    
+    #Create AI players
+    ai1 =  AIPlayer("test 1","lo") 
+    ai2 =  AIPlayer("test 2","lo")
+    ai3 = AIPlayer("test 3","mid")
+    ai4 = AIPlayer("test 4","hi")
+    
+    #aiplay() Returns an AI player
+    self.assertIsInstance(aiplay(ai1,ai2),AIPlayer)
 
 if __name__ == '__main__':
     unittest.main()
