1f2e5f99dfcf1e97b1acd5282fd5cdc654d01919
AndreasFWallin <h.andreas.wallin@gmail.com>
2020-02-20 22:33:31 +0100

    game_manager can now deal with json and rad from file for board

--- tests/test_game_manager.py
+++ tests/test_game_manager.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, r"C:\UU\VT20\SEPM\ProjectSE")
+sys.path.insert(0, r"C:\UU\VT20\SEPM\updated\ProjectSE")
 import unittest
 from projectse.game_manager import GameManager as GM
 
@@ -7,12 +7,44 @@ class TestGameManager(unittest.TestCase):
 
     def test_game_manager_string_naive(self):
         """
-        Testing if the game_manager can connect to the 
+        Testing if the game_manager can connect to the server
         """
         gm = GM()
         message = "1,2 1,3"
         gm.send(message)
-        self.assertEquals(gm.recv(dtype="string"), message)
+        recv_msg =  gm.recv(dtype="string")
+        self.assertEqual(recv_msg, message)
+        gm.close()
+    def test_game_manager_json_naive(self):
+        """
+        Testing if the game_manager can connect to the server and send and recieve a json message
+        """
+        gm = GM()
+        gm.send_json()
+        recv_msg = gm.recv_json(dtype="json")
+        self.assertEqual(recv_msg["diff"], 1)
+        gm.close()
+
+    def test_game_manager_json_server(self):
+        """
+        Testing if the game_manager can connect to the server and the server can change the 
+        """
+        gm = GM()
+        message_board = [1]*24
+        gm.send_json(message_board)
+        recv_msg = gm.recv_json(dtype="json")
+        message_board[0] = 2
+        self.assertEqual(recv_msg["board"], message_board)
+        gm.close()
+
+    def test_game_manager_json_txt_naive(self):
+        """
+        Testing if the game_manager can connect to the 
+        """
+        gm = GM()
+        gm.send_json_txt_file()
+        recv_msg =  gm.recv_json(dtype="json")
+        self.assertEqual(recv_msg["diff"], 1)
         gm.close()
 
 if __name__ == '__main__':
