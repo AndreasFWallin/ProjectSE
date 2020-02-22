95f27c59579f213104a9b1cfbd980c62e4a1ac1d
gustafffandersson <48953910+gustafffandersson@users.noreply.github.com>
2020-02-22 14:43:52 +0100

    Update tournament_scheduler.py

--- projectse/tournament_scheduler.py
+++ projectse/tournament_scheduler.py
@@ -1,41 +1,46 @@
+""" Creating a tournament schedule consisting of an array (schedule)
+    containing arrays with tuples, representing matches. """
 class TournamentScheduler:
     def __init__(self, players):
         self.num_players = players
         self.schedule = []
 
-        """ Creating a tournament schedule consisting of an array (schedule)
-            containing arrays with tuples, representing matches. """
-        teams = []
-        for i in range(1,players+1):
-            teams.append(i)
-
-        if (players % 2) == 1:
-            teams.append(0)
-            for round in range(1,players):
-                matches = []
-                for i in range(len(teams) / 2):
-                    if (teams[i] != 0) and (teams[len(teams) - i - 1] != 0):
-                        if (round % 2) == 0:
-                            matches.append((teams[i], teams[len(teams) - i - 1]))
-                            teams.insert(1, teams.pop())
-                            schedule.append(matches)
-                        else:
-                            matches.append((teams[len(teams) - i - 1], teams[i]))
-                            teams.insert(1, teams.pop())
-                            schedule.append(matches)
-
-        else:
-            for round in range(players - 1):
-                matches = ()
-                for i in range(len(teams) / 2):
-                    if (round % 2) == 0:
-                        matches.append((teams[i], teams[len(teams) - i - 1]))
-                        teams.insert(1, teams.pop())
-                        schedule.append(matches)
-                    else:
-                        matches.append((teams[len(teams) - i - 1], teams[i]))
-                        teams.insert(1, teams.pop())
-                        schedule.append(matches)
+        if (players == 3):
+            self.schedule.append([(2,3)])
+            self.schedule.append([(1, 2)])
+            self.schedule.append([(3, 1)])
+        elif (players == 4):
+            self.schedule.append([(1, 4), (2, 3)])
+            self.schedule.append([(4, 3), (1, 2)])
+            self.schedule.append([(2, 4), (3, 1)])
+        elif (players == 5):
+            self.schedule.append([(2, 5), (3, 4)])
+            self.schedule.append([(5, 3), (1, 2)])
+            self.schedule.append([(3, 1), (4, 5)])
+            self.schedule.append([(1, 4), (2, 3)])
+            self.schedule.append([(4, 2), (5, 1)])
+        elif (players == 6):
+            self.schedule.append([(1, 6), (2, 5), (3, 4)])
+            self.schedule.append([(6, 4), (5, 3), (1, 2)])
+            self.schedule.append([(2, 6), (3, 1), (4, 5)])
+            self.schedule.append([(6, 5), (1, 4), (2, 3)])
+            self.schedule.append([(3, 6), (4, 2), (5, 1)])
+        elif (players == 7):
+            self.schedule.append([(2, 7), (3, 6), (4, 5)])
+            self.schedule.append([(6, 4), (7, 3), (1, 2)])
+            self.schedule.append([(3, 8), (4, 7), (5, 6)])
+            self.schedule.append([(7, 5), (1, 4), (2, 3)])
+            self.schedule.append([(4, 2), (5, 1), (6, 7)])
+            self.schedule.append([(1, 6), (2, 5), (3, 4)])
+            self.schedule.append([(5, 3), (6, 2), (7, 1)])
+        elif (players == 8):
+            self.schedule.append([(1, 8), (2, 7), (3, 6), (4, 5)])
+            self.schedule.append([(8, 5), (6, 4), (7, 3), (1, 2)])
+            self.schedule.append([(2, 8), (3, 1), (4, 7), (5, 6)])
+            self.schedule.append([(8, 6), (7, 5), (1, 4), (2, 3)])
+            self.schedule.append([(3, 8), (4, 2), (5, 1), (6, 7)])
+            self.schedule.append([(8, 7), (1, 6), (2, 5), (3, 4)])
+            self.schedule.append([(4, 8), (5, 3), (6, 2), (7, 1)])
 
 
     """ get_round takes an integer (1-7) as an input and returns
